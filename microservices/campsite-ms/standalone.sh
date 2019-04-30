fff580ed7d1ed3682493467eaa7d3e6724924a8e
Mauricio Aiello <maiello@temperies.com>
2019-04-30 17:32:15 -0300

    Removing mvnw warapper - use mvn instead

--- microservices/campsite-ms/standalone.sh
+++ microservices/campsite-ms/standalone.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 #export MAVEN_OPTS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8002";
 #mvn clean spring-boot:run -Dspring-boot.run.arguments=--server.port=8082
-../mvnw clean spring-boot:run
+mvn clean spring-boot:run
--- microservices/eureka-server/standalone.sh
+++ microservices/eureka-server/standalone.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 #export MAVEN_OPTS="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8008";
-../mvnw clean spring-boot:run
+mvn clean spring-boot:run
 
