969ad26a2a61a7239da0ee8dbb3a87c8b0b85dfc
Mauricio Aiello <maiello@temperies.com>
2019-05-14 11:58:16 -0300

    Update spring-boot version && add dashboard monitor

--- microservices/dashboard-server/src/main/java/com/improve/reservations/dashboardserver/SecurityConfig.java
+++ microservices/dashboard-server/src/main/java/com/improve/reservations/dashboardserver/SecurityConfig.java
@@ -5,24 +5,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final String adminContextPath;
+
+	public SecurityConfig(AdminServerProperties adminServerProperties) {
+		this.adminContextPath = adminServerProperties.getContextPath();
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
 		successHandler.setTargetUrlParameter("redirectTo");
-		successHandler.setDefaultTargetUrl("/");
 
-		http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
-		http.logout().logoutUrl("/logout");
-		// Login page and static assets should not be protected
-		http.authorizeRequests().antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**").permitAll();
-		// Everything else requires authentication
-		http.authorizeRequests().antMatchers("/**").authenticated();
-		http.httpBasic();
-		// 1.x does not support csrf
-		http.csrf().disable();
+		http.authorizeRequests().antMatchers(adminContextPath + "/assets/**").permitAll()
+				.antMatchers(adminContextPath + "/login").permitAll().anyRequest().authenticated().and().formLogin()
+				.loginPage(adminContextPath + "/login").successHandler(successHandler).and().logout()
+				.logoutUrl(adminContextPath + "/logout").and().httpBasic().and().csrf().disable();
+
 	}
 
 }
