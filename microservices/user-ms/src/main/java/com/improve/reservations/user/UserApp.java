6ef1f99f7f2dc0885b150865842ae2e0ce948765
aiellomau <mauricio.aiello@gmail.com>
2019-06-08 20:06:14 -0300

    Refresh cnfig and dashboard updated

--- microservices/user-ms/src/main/java/com/improve/reservations/user/UserApp.java
+++ microservices/user-ms/src/main/java/com/improve/reservations/user/UserApp.java
@@ -5,12 +5,14 @@ import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @ComponentScan
 @EnableAutoConfiguration
 @EnableDiscoveryClient
+@RefreshScope
 @Component
 public class UserApp {
 
